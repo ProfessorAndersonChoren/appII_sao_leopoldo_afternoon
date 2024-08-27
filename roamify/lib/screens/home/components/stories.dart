aa6a2ecd6777a1246642818866e219990c2e6f0d
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2024-08-27 17:05:14 -0300

    App Roamify - Tela inicial - AppBar

---
+++ roamify/lib/screens/home/components/stories.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Stories extends StatelessWidget {
+  const Stories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Text('Stories'),
+        ],
+      ),
+    );
+  }
+}
